f1d1030dd45dd18b6288009d86e76648dad7fff2
JakubKowalski1997 <kubabartek1@op.pl>
2019-04-15 19:40:20 +0200

    only first administrator is enabled to register without confirmation (concept)

--- src/main/java/com/dnsite/security/user/service/UserService.java
+++ src/main/java/com/dnsite/security/user/service/UserService.java
@@ -2,9 +2,13 @@ package com.dnsite.security.user.service;
 
 import com.dnsite.security.user.model.User;
 
+import java.util.List;
+
 public interface UserService {
 
     void save(User user);
 
     User findByUsername(String username);
+
+    List<User> findAll();
 }
