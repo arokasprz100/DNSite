ac6aa3ac4fdf148d1216d5137c4953122a05df54
JakubKowalski1997 <kubabartek1@op.pl>
2019-04-15 18:35:54 +0200

    database need some more attributes

--- src/main/java/com/dnsite/security/user/model/User.java
+++ src/main/java/com/dnsite/security/user/model/User.java
@@ -28,6 +28,24 @@ public class User {
 
     private Date lastLoginDate;
 
+    private String email;
+
+    private String hostname;
+
+    private Long dbPort;
+
+    private String database;
+
+    private String powerAdminPassword;
+
+    private String powerAdminUsername;
+
+    private String hostmaster;
+
+    private String primaryNameServer;
+
+    private String secondNameServer;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
@@ -103,5 +121,77 @@ public class User {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public Long getDbPort() {
+        return dbPort;
+    }
+
+    public void setDbPort(Long dbPort) {
+        this.dbPort = dbPort;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getPowerAdminPassword() {
+        return powerAdminPassword;
+    }
+
+    public void setPowerAdminPassword(String powerAdminPassword) {
+        this.powerAdminPassword = powerAdminPassword;
+    }
+
+    public String getPowerAdminUsername() {
+        return powerAdminUsername;
+    }
+
+    public void setPowerAdminUsername(String powerAdminUsername) {
+        this.powerAdminUsername = powerAdminUsername;
+    }
+
+    public String getHostmaster() {
+        return hostmaster;
+    }
+
+    public void setHostmaster(String hostmaster) {
+        this.hostmaster = hostmaster;
+    }
+
+    public String getPrimaryNameServer() {
+        return primaryNameServer;
+    }
+
+    public void setPrimaryNameServer(String primaryNameServer) {
+        this.primaryNameServer = primaryNameServer;
+    }
+
+    public String getSecondNameServer() {
+        return secondNameServer;
+    }
+
+    public void setSecondNameServer(String secondNameServer) {
+        this.secondNameServer = secondNameServer;
     }
 }
