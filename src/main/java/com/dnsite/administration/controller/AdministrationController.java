2532b003b67ac8a0ff646d799e61e6a4176ff935
arokasprz100 <arkadiusz.kasprzak@onet.pl>
2019-04-24 21:53:09 +0200

    confirm/reject user migration to React

--- src/main/java/com/dnsite/administration/controller/AdministrationController.java
+++ src/main/java/com/dnsite/administration/controller/AdministrationController.java
@@ -7,10 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,26 +24,28 @@ public class AdministrationController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/user-confirm", method = RequestMethod.GET)
-    public String userConfirmation(Model model){
+    @RequestMapping(value = "/user-confirm/all", method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> userConfirmation(){
         List<User> usersToConfirm = userService.findAll()
                 .stream().filter(a -> !a.getRole().equals("ADMIN"))
                 .collect(Collectors.toList());
-        model.addAttribute("userForConfirmation", usersToConfirm);
-        return "user-confirm";
+        return usersToConfirm;
     }
 
     @RequestMapping(value = "/user-confirm/{id}/accept", method = RequestMethod.GET)
+    @ResponseBody
     public String userConfirmationAccepted(@PathVariable Long id){
         userService.updateUserRole(id, Role.ADMIN);
         logger.info("User with id: " + id + " confirmed");
-        return "redirect:/administration/user-confirm";
+        return "User accepted";
     }
 
     @RequestMapping(value = "/user-confirm/{id}/reject", method = RequestMethod.GET)
+    @ResponseBody
     public String userConfirmationRejected(@PathVariable Long id){
         userService.deleteUserById(id);
         logger.info("User with id: " + id + " is rejected and deleted;");
-        return "redirect:/administration/user-confirm";
+        return "User rejected";
     }
 }
--- src/main/webapp/src/resources/pages/Administration.js
+++ src/main/webapp/src/resources/pages/Administration.js
@@ -1,24 +1,111 @@
 import React from "react";
 import "../styles/Administration.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
-const Administration = () => {
-    var customObjectList = JSON.parse('${usersToConfirm}');
-    console.log(customObjectList);
-  return <div className="container">
+class Administration extends React.Component {
 
-      {/*<form:form method="POST" modelAttribute="userForConfirmation">*/}
-          {/*<c:forEach items="${userForConfirmation}" var="user">*/}
-              {/*<tr>*/}
-                  {/*<td>this.state.user.id</td>*/}
-                  {/*/!*<td>${user.username}</td>*!/*/}
-                  {/*/!*<td>${user.email}</td>*!/*/}
-                  {/*<td><a href="/administration/user-confirm/${user.id}/accept">accept</a></td>*/}
-                  {/*<td><a href="/administration/user-confirm/${user.id}/reject">reject</a></td>*/}
-              {/*</tr>*/}
-          {/*</c:forEach>*/}
-      {/*</form:form>*/}
+    constructor(props) {
+        super(props);
 
-  </div>;
-};
+        console.log("Constructor");
+
+        this.state = {
+            data : []
+        };
+    }
+
+    refreshTable() {
+        console.log(this.state.data);
+        fetch('http://localhost:8001/administration/user-confirm/all')
+            .then(response => {
+                if (response.ok) {
+                    return response;
+                }
+            throw Error(response.status);
+            })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({data: data, selected: {}});
+            })
+        .catch(error => console.log(error + " coś nie tak"));
+    }
+
+    componentDidMount() {
+        console.log("Mount");
+        console.log(this.state.data);
+        this.refreshTable();
+    }
+
+    acceptUser = (userId) => {
+        let self = this;
+        let URI = 'http://localhost:8001/administration/user-confirm/' + userId  + '/accept';
+        fetch(URI)
+        .then(function(response) {
+            return response;
+        }).then(function(data) {
+            self.refreshTable();
+        });
+    }
+
+    rejectUser = (userId) => {
+        let self = this;
+        let URI = 'http://localhost:8001/administration/user-confirm/' + userId  + '/reject';
+        fetch(URI)
+        .then(function(response) {
+            return response;
+        }).then(function(data) {
+            self.refreshTable();
+        });
+
+    }
+
+    renderTable = ()  => {
+        const columns = [
+        {
+            Header : "Username",
+            accessor: 'username',
+        },
+        {
+            Header : "Email",
+            accessor: 'email',
+        },
+        {
+            Header : "Accept",
+            accessor: "",
+            Cell : ({original}) => {
+                return (
+                    <button onClick = { () => {this.acceptUser(original.id)}} > Accept </button>
+                );
+            },
+            sortable: false
+        },
+        {
+            Header : "Reject",
+            accessor : "",
+            Cell : ({original}) => {
+                return (
+                    <button onClick = { () => {this.rejectUser(original.id)}} > Reject </button>
+                );
+            },
+            sortable: false
+        }
+        ];
+
+        return (
+            <ReactTable
+                data={this.state.data}
+                columns={columns}
+                defaultSorted={ [ { id : "username", desc: true} ] }
+            />
+        );
+    }
+
+    render() {
+        return this.renderTable();
+    }
+
+}
 
 export default Administration;
