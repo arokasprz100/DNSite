b5e3fe98e119aaf0c5e92d638798065c71e2ba69
Jakub Kowalski <kubabartek1@op.pl>
2019-05-20 13:13:18 +0200

    delete unnecesary user attrbutes (moved to dbconfig)

--- src/main/java/com/dnsite/utils/hibernate/DbConfig.java
+++ src/main/java/com/dnsite/utils/hibernate/DbConfig.java
@@ -2,7 +2,21 @@ package com.dnsite.utils.hibernate;
 
 public class DbConfig {
     private String username;
+
     private String password;
+
+    private String hostname;
+
+    private Long dbPort;
+
+    private String database;
+
+
+    private String hostmaster;
+
+    private String primaryNameServer;
+
+    private String secondNameServer;
 
     public String getUsername() {
         return username;
@@ -18,5 +32,53 @@ public class DbConfig {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public Long getDbPort() {
+        return dbPort;
+    }
+
+    public void setDbPort(Long dbPort) {
+        this.dbPort = dbPort;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getHostmaster() {
+        return hostmaster;
+    }
+
+    public void setHostmaster(String hostmaster) {
+        this.hostmaster = hostmaster;
+    }
+
+    public String getPrimaryNameServer() {
+        return primaryNameServer;
+    }
+
+    public void setPrimaryNameServer(String primaryNameServer) {
+        this.primaryNameServer = primaryNameServer;
+    }
+
+    public String getSecondNameServer() {
+        return secondNameServer;
+    }
+
+    public void setSecondNameServer(String secondNameServer) {
+        this.secondNameServer = secondNameServer;
     }
 }
