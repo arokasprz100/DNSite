5de06298df79d55e7fe6acee5450b40357f2cf53
arokasprz100 <arkadiusz.kasprzak@onet.pl>
2019-06-14 16:58:55 +0200

    Logout

--- src/main/webapp/src/resources/layouts/UserBlock.js
+++ src/main/webapp/src/resources/layouts/UserBlock.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
 import "../styles/UserBlock.css";
+import { Redirect } from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 
 class UserBlock extends Component {
-  state = { userName: "User" };
+  state = {
+      redirect: false
+    }
+    setRedirect = () => {
+      this.setState({
+        redirect: true
+      })
+    }
+    renderRedirect = () => {
+      if (this.state.redirect) {
+        return <Redirect to='/privacy-policy' />
+      }
+    }
   render() {
     return (
       <div className="userBlock">
         <h4>
-          Hello <span>{this.state.userName}</span>!
+          Hello <span>User</span>!
         </h4>
         <div className="userButtons">
-          <button onclick="location.href = 'localhost:8001/login';">Ustawienia konta</button>|<button>Wyloguj</button>
-          <button>Change password</button><button>Logout</button>
+          <button>Profile settings</button>
+          <button>Change password</button>
+          {this.renderRedirect()}
+          <button onClick={this.setRedirect}>Logout</button>
+          <Route path = '/privacy-policy' component={() => {
+          window.location.href = 'http://localhost:8001/logout';
+          return null;
+          }}/>
         </div>
       </div>
     );
