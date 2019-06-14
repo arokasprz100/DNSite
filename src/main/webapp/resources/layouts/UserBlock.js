4e885f64b59b09f7397a39bc3265320ae34865cb
Krystian Molenda <krystian.molenda@gmail.com>
2019-06-14 18:01:37 +0200

    logout

--- src/main/webapp/resources/layouts/UserBlock.js
+++ src/main/webapp/resources/layouts/UserBlock.js
@@ -1,20 +1,33 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "../styles/UserBlock.css";
+import { withRouter } from 'react-router-dom';
+import { Button, Card, CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row, NavLink  } from 'reactstrap';
 
 class UserBlock extends Component {
-  state = { userName: "Example" };
-  render() {
-    return (
-      <div className="userBlock">
-        <h3>
-          Witaj <span>{this.state.userName}</span>!
-        </h3>
-        <div className="userButtons">
-          <button>Ustawienia konta</button>|<button>Wyloguj</button>
-        </div>
-      </div>
-    );
-  }
+    constuctor() {
+        this.routeChange = this.routeChange.bind(this);
+    }
+
+    routeChange() {
+        let path = `localhost:8001/login`;
+        this.props.history.push(path);
+    }
+
+    state = {userName: "Example"};
+
+    render() {
+        return (
+            <div className="userBlock">
+                <h3>
+                    Witaj <span>{this.state.userName}</span>!
+                </h3>
+                    <div className="userButtons">
+                    <button>Ustawienia konta</button>
+                    <button onClick={this.routeChange}>Wyloguj</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default UserBlock;
