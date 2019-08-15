1aa74ed4c9be1deb197170ab1b01a5b6db4ba43f
Krystian Molenda <krystian.molenda@gmail.com>
2019-08-15 14:05:35 +0200

    fix password generator

--- src/test/java/com/dnsite/security/user/utils/PasswordGeneratorTest.java
+++ src/test/java/com/dnsite/security/user/utils/PasswordGeneratorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 
 public class PasswordGeneratorTest {
 
+
     @Test
     public void testPasswordGenerator(){
 
@@ -13,11 +14,10 @@ public class PasswordGeneratorTest {
                 .useLower(true)
                 .useUpper(true)
                 .build();
-        String tmpPasswd = passwordGenerator.generate(7);
-        assertTrue(tmpPasswd.length() == 7);
-        assertTrue(tmpPasswd.matches(".*[0-9]+.*")); //useDigits
-        assertTrue(tmpPasswd.matches(".*[A-Z]+.*")); // useUper
-        assertTrue(tmpPasswd.matches(".*[a-z]+.*")); // useLower
-
+        String tmpPasswd = passwordGenerator.generate(15);
+        assertTrue("wrong len: " + tmpPasswd,tmpPasswd.length() == 15);
+        assertTrue("no digits: "+ tmpPasswd, tmpPasswd.matches(".*[0-9]+.*")); //useDigits
+        assertTrue("no upper case: "+ tmpPasswd , tmpPasswd.matches(".*[A-Z]+.*")); // useUper
+        assertTrue("no lower case: " + tmpPasswd, tmpPasswd.matches(".*[a-z]+.*")); // useLower
     }
 }
