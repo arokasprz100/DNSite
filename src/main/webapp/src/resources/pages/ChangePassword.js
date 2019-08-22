ecf04f63f2f64a0f8ec2d74762174aa96caf622d
Pasik97 <k.pasik97@gmail.com>
2019-08-22 18:53:25 +0200

    added textarea styles, word wrap for cells added

--- src/main/webapp/src/resources/pages/ChangePassword.js
+++ src/main/webapp/src/resources/pages/ChangePassword.js
@@ -23,11 +23,11 @@ class ChangePassword extends React.Component {
             <div className="changePasswordWrapper">
                 <div className="inputsWrapper">
                     <div className="oldPasswordWrapper">
-                        <input value={this.state.oldPassword} placeholder="Old password" onChange={this.handleChange} name="oldPassword"/>
+                        <input type="password" value={this.state.oldPassword} placeholder="Old password" onChange={this.handleChange} name="oldPassword"/>
                     </div>
                     <div className="newPasswordWrapper">
-                        <input value={this.state.newPassword} placeholder="New password" onChange={this.handleChange} name="newPassword"/>
-                        <input value={this.state.newPasswordConfirm} placeholder="Repeat password" onChange={this.handleChange} name="newPasswordConfirm"/>
+                        <input type="password" value={this.state.newPassword} placeholder="New password" onChange={this.handleChange} name="newPassword"/>
+                        <input type="password" value={this.state.newPasswordConfirm} placeholder="Repeat password" onChange={this.handleChange} name="newPasswordConfirm"/>
                     </div>
                 </div>
                 <button onClick={this.onSubmit}>Change password</button>
